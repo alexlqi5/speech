441ea87865deccb51615ec96eb0abde1b8a6c500
alexlqi5 <131314641+alexlqi5@users.noreply.github.com>
2023-05-01 19:04:43 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [transcript, setTranscript] = useState('');
+  const [isListening, setIsListening] = useState(false);
+
+  useEffect(() => {
+    const recognition = new window.webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+
+    recognition.onresult = (event) => {
+      const transcript = Array.from(event.results)
+        .map((result) => result[0])
+        .map((result) => result.transcript)
+        .join('');
+
+      setTranscript(transcript);
+    };
+
+    recognition.onend = () => {
+      setIsListening(false);
+    };
+
+    if (isListening) {
+      recognition.start();
+    } else {
+      recognition.stop();
+    }
+
+    return () => {
+      recognition.stop();
+    };
+  }, [isListening]);
+
+  const handleToggleListen = () => {
+    setIsListening(!isListening);
+  };
+
+  return (
+    <div>
+      <button onClick={handleToggleListen}>
+        {isListening ? 'Stop' : 'Start'} Listening
+      </button>
+      <p>{transcript}</p>
+    </div>
+  );
+}
+
+export default App;
